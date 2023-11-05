d1f896499c1f0dac031e7474a33b7a3cdb15e8e8
Vishal Kumar Gupta <vishaaal05@gmail.com>
2023-11-05 14:37:00 +0530

    v1-blog-api

--- src/main/java/com/rebuiltx/blog/config/SecurityConfig.java
+++ src/main/java/com/rebuiltx/blog/config/SecurityConfig.java
@@ -7,7 +7,6 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.authentication.configuration.EnableGlobalAuthentication;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -21,7 +20,6 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-import com.rebuiltx.blog.exceptions.CustomAccessDeniedHandler;
 import com.rebuiltx.blog.security.CustomUserDetailService;
 import com.rebuiltx.blog.security.JwtAuthenticationEntryPoint;
 import com.rebuiltx.blog.security.JwtAuthenticationFilter;
